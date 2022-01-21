47838cf37281c2ffead27deca25870550c0e842d
PRinCeThePro69 <78784282+PRinCeThePro69@users.noreply.github.com>
2022-01-21 22:58:10 +0600

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -12,8 +12,8 @@ client.on("messageCreate", async (msg) => {
 
         if(msg.channel.id === "934082965774925824") {
 
-            msg.member.roles.add("934082996481437746")
-            new sentmsgSchema = ({
+            msg.member.roles.add("934082996481437746");
+            new sentmsgSchema ({
                 userId: msg.author.id,
                 sent: true,
             }).save(); 
