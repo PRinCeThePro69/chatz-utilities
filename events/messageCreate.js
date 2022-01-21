ebc5c0ffcb271ecff2e9afedd1ed6e4c31e6ddba
PRinCeThePro69 <78784282+PRinCeThePro69@users.noreply.github.com>
2022-01-21 23:09:14 +0600

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,29 +1,38 @@
 const client = require("../index");
-const { Client, Collection, MessageEmbed, MessageActionRow, MessageButton  } = require("discord.js");
+const {
+    Client,
+    Collection,
+    MessageEmbed,
+    MessageActionRow,
+    MessageButton
+} = require("discord.js");
 const sentmsgSchema = require('../models/sentonemsg');
 
 
 client.on("messageCreate", async (msg) => {
     if (
         msg.author.bot ||
-        !msg.guild 
-				    )
+        !msg.guild
+    )
         return;
 
-        if(msg.channel.id === "934082965774925824") {
+    if (msg.channel.id === "934082965774925824") {
 
-            msg.member.roles.add("934082996481437746");
-            new sentmsgSchema ({
-                userId: msg.author.id,
-                sent: true,
-            }).save(); 
-        }    
+        msg.member.roles.add("934082996481437746");
+        new sentmsgSchema({
+            userId: msg.author.id,
+            sent: true,
+        }).save();
+    }
 });
 client.on('guildMemberAdd', (mem) => {
- const sent =   sentmsgSchema.findOne({
+    const didsent = sentmsgSchema.findOne({
         userId: mem.id,
         sent: true
     });
-    if(sent && sent.length) { mem.roles.add("934082996481437746")}
+    if (didsent && didsent.length) {
+        mem.roles.add("934082996481437746")
+    };
+    
 })
 // auto mod stuff
