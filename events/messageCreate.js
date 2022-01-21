04e83b0c1fd1582c572066a0d23f87952329236f
PRinCeThePro69 <78784282+PRinCeThePro69@users.noreply.github.com>
2022-01-21 22:57:00 +0600

    sent msg

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,7 @@
 const client = require("../index");
 const { Client, Collection, MessageEmbed, MessageActionRow, MessageButton  } = require("discord.js");
-const warnSchema = require('../models/automodSchema')
+const sentmsgSchema = require('../models/sentonemsg');
+
 
 client.on("messageCreate", async (msg) => {
     if (
@@ -10,7 +11,19 @@ client.on("messageCreate", async (msg) => {
         return;
 
         if(msg.channel.id === "934082965774925824") {
+
             msg.member.roles.add("934082996481437746")
-        }
+            new sentmsgSchema = ({
+                userId: msg.author.id,
+                sent: true,
+            }).save(); 
+        }    
 });
+client.on('guildMemberAdd', (mem) => {
+ const sent =   sentmsgSchema.findOne({
+        userId: mem.id,
+        sent: true
+    })
+    if(sent) mem.roles.add("934082996481437746")
+})
 // auto mod stuff
