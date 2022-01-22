fb329191d495a9b81219d55b251e97a7302f9085
SirEpiclyAmazing <74148830+SirEpiclyAmazing@users.noreply.github.com>
2022-01-22 16:37:57 +0000

    clean code

--- automod/anti-link.js
+++ automod/anti-link.js
@@ -1,9 +1,5 @@
 const { MessageEmbed } = require('discord.js')
-
-
-  
 module.exports = (client) => {
-
  function isValidURL(string) {
         var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
         return (res !== null)
@@ -14,8 +10,7 @@ module.exports = (client) => {
         let guilld = client.guilds.cache.get('930503731974385694');
     let logch = guilld.channels.cache.get('931558609194737786')
       var testContent = msg.content;
-      if(isValidURL(testContent) && !msg.member.permissions.has('ADMINISTRATOR')) {
-		
+      if(isValidURL(testContent) && !msg.member.permissions.has('ADMINISTRATOR')) {	
            const log = new MessageEmbed()
         .setTitle('New Mute!')
         .setColor('RED')
@@ -25,13 +20,10 @@ module.exports = (client) => {
         .setTimestamp()
           const e = new MessageEmbed()
         .setColor('RED')
-				.setDescription(`${msg.author} You are not allowed to send external links in this server!`)
-
+	.setDescription(`${msg.author} You are not allowed to send external links in this server!`)
         msg.channel.send({
-					embeds: [e]
-    
-          
-				})
+	embeds: [e]
+	})
         msg.member.timeout(30 * 60 * 1000, '[Automod] Advertising').catch((e) => console.log(e))
         logch.send({embeds: [log]})
         msg.delete().catch((e))
@@ -43,14 +35,9 @@ module.exports = (client) => {
                 .addField('Reason' , '[AutoMod] Sending external links which is not allowed in this server.')
                 .addField('Expires' , '30 minutes')
                     msg.author.send({embeds: [yougotmuted]})
- 
                 }catch(err) {
                     console.log(err)
                 }
       }
-
-
-
-
 })
 }
