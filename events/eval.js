c003215f7e497d4399d6c701c2ad8f8cacbe5957
PRinCeThePro69 <78784282+PRinCeThePro69@users.noreply.github.com>
2022-01-25 21:57:22 +0600

    Update eval.js

--- events/eval.js
+++ events/eval.js
@@ -11,33 +11,33 @@ const prefix = '>'
 
 client.on("messageCreate", async (msg) => {
     const args = msg.content.slice(prefix.length).trim().split(' ');
-    
+    args.shift()
+    const code = args.join(" ");
    
     if(msg.content.startsWith('>eval')) {
         if(msg.member.id !== '689173890450194434' && msg.member.id !== '593696963061481532' ) return msg.delete();
-      
-        const args = msg.content.slice(prefix.length).trim().split(' ');
-        args.shift()
-        const code = args.join(" ");
         if(!code) return msg?.delete()
-            msg.delete()
-            function clean(text) {
-                if (typeof(text) === "string")
-                  return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-                else
-                    return text;
-              }
-            try {
-             
-              let evaled = eval(code);
-        
-              if (typeof evaled !== "string")
-                evaled = require("util").inspect(evaled);
-        
-             msg.channel.send(clean(evaled), {code:"xl"});
-            } catch (err) {
-              msg.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-            }
+        msg?.delete()
+
+    // function clean(text) {
+    //     if (typeof(text) === "string")
+    //       return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+        // else
+    //         return text;
+    //   }
+    try {
+     
+      let evaled = eval(code);
+
+      
+
+      
+    } catch (err) {
+     return msg.channel.send(`\`\`\`${err}\`\`\``);
+    }
+    if (typeof evaled !== "string")
+    evaled = require("util").inspect(evaled);
+    msg.channel.send(`\`\`\`${evaled}\`\`\``);
 
 
 }
