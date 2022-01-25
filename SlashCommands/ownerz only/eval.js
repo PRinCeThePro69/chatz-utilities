e16869b36ace2a95bb9f1b1b2b46c3a0623f0155
PRinCeThePro69 <78784282+PRinCeThePro69@users.noreply.github.com>
2022-01-25 15:02:42 +0600

    Update eval.js

--- SlashCommands/ownerz only/eval.js
+++ SlashCommands/ownerz only/eval.js
@@ -1,46 +1,46 @@
-const { Client, CommandInteraction, MessageEmbed, MessageSelectMenu } = require("discord.js");
+// const { Client, CommandInteraction, MessageEmbed, MessageSelectMenu } = require("discord.js");
 
-module.exports = {
-    name: "eval", //are there aliases?
-    description: "Run a code for the bot from discord. (Owner only)",
-    options: [
-      {
-        name: 'code',
-        description: 'The code to run.',
-        type: 'STRING',
-        required: true,
-      },
-    ],
+// module.exports = {
+//     name: "eval", //are there aliases?
+//     description: "Run a code for the bot from discord. (Owner only)",
+//     options: [
+//       {
+//         name: 'code',
+//         description: 'The code to run.',
+//         type: 'STRING',
+//         required: true,
+//       },
+//     ],
     
-    /**
-     *
-     * @param {Client} client
-     * @param {CommandInteraction} interaction
-     * @param {String[]} args
-     */
-    run: async (client, interaction, args) => {
+//     /**
+//      *
+//      * @param {Client} client
+//      * @param {CommandInteraction} interaction
+//      * @param {String[]} args
+//      */
+//     run: async (client, interaction, args) => {
        
-      const code = interaction.options.getString('code')
-      if (interaction.member.id !== '689173890450194434' || !interaction.member.id !== '593696963061481532') return;
-            function clean(text) {
-                if (typeof(text) === "string")
-                  return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-                else
-                    return text;
-              }
-            try {
+//       const code = interaction.options.getString('code')
+//       if (interaction.member.id !== '689173890450194434' || !interaction.member.id !== '593696963061481532') return;
+//             function clean(text) {
+//                 if (typeof(text) === "string")
+//                   return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+//                 else
+//                     return text;
+//               }
+//             try {
              
-              let evaled = eval(code);
+//               let evaled = eval(code);
         
-              if (typeof evaled !== "string")
-                evaled = require("util").inspect(evaled);
+//               if (typeof evaled !== "string")
+//                 evaled = require("util").inspect(evaled);
         
-              interaction.channel.send(clean(evaled), {code:"xl"});
-            } catch (err) {
-              interaction.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-            }
-    },
-};
+//               interaction.channel.send(clean(evaled), {code:"xl"});
+//             } catch (err) {
+//               interaction.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+//             }
+//     },
+// };
 
 
       
