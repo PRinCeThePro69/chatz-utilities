643d990a5199bfde5be0f0329e10f54c72a7eae8
PRinCeThePro69 <78784282+PRinCeThePro69@users.noreply.github.com>
2022-01-25 17:26:01 +0600

    Update eval.js

--- events/eval.js
+++ events/eval.js
@@ -11,7 +11,7 @@ const prefix = '>'
 
 client.on("messageCreate", async (msg) => {
     const args = msg.content.slice(prefix.length).trim().split(' ');
-    i
+    
     if(msg.content.startsWith('>eval')) {
         if(msg.member.id !== '689173890450194434' || msg.member.id !== '593696963061481532' ) return msg.delete();
     msg.delete()
