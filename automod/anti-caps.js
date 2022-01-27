ee181bafc2f6306aa0c46bb46d9ea2f2903144b0
PRinCeThePro69 <78784282+PRinCeThePro69@users.noreply.github.com>
2022-01-27 22:28:56 +0600

    Update anti-caps.js

--- automod/anti-caps.js
+++ automod/anti-caps.js
@@ -21,7 +21,7 @@ module.exports = (client) => {
       
         const textCaps = (caps / msg.content.length) * 100;
         // Gets a percentage of the capital letters.
-      console.log((caps / msg.content.length))
+      console.log(caps)
         if (textCaps >= 75) {
         // If the capital letters is over or equals to 60% of the message,
         // and if the user isn't an ADMINISTRATOR, then...
