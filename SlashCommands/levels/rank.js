e899305191138531281bb82c38cbd8107d8d0f56
PRinCeThePro69 <78784282+PRinCeThePro69@users.noreply.github.com>
2022-02-03 23:52:14 +0600

    Update rank.js

--- SlashCommands/levels/rank.js
+++ SlashCommands/levels/rank.js
@@ -18,15 +18,15 @@ module.exports = {
         const user = Levels.fetch(interaction.user.id, interaction.guild.id)
             
             const rank = new canvacord.Rank() // Build the Rank Card
-            .setAvatar(target.displayAvatarURL({format: 'png', size: 512, dynamic: false}))
+            .setAvatar(interaction.user.displayAvatarURL({format: 'png', size: 512, dynamic: false}))
             .setCurrentXP(user.xp) // Current User Xp
             .setRequiredXP(Levels.xpFor(user.level + 1)) // We calculate the required Xp for the next level
             .setRank(user.position) // Position of the user on the leaderboard
             .setLevel(user.level) // Current Level of the user
             .setStatus(target.presence.status)
             .setProgressBar("#00FFFF")
-            .setUsername(target.username)
-            .setDiscriminator(target.discriminator);
+            .setUsername(interaction.user.username)
+            .setDiscriminator(interaction.user.discriminator);
 
             rank.build().then(data => {
                 const attachment = new Discord.MessageAttachment(data, `${target.tag}.png`);
